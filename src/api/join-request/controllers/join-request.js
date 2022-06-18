6e9015b6cd8ad00f3f0fcef4e1b0e93a4ea875f5
amrahli <amrahli@hotmail.com>
2022-06-18 12:46:30 +0400

    join request schema

---
+++ src/api/join-request/controllers/join-request.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  join-us controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::join-request.join-request');
